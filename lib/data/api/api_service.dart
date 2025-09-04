7b4433d0cc121b545dbb9378b0095131124fde04
miftahasiboean <miftaheczson027@gmail.com>
2025-09-04 10:53:53 +0700

    add api service

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,1 +1,29 @@
-class ApiService {}
+import 'dart:convert';
+
+import 'package:restaurant_app/data/model/restaurant_detail_respone.dart';
+import 'package:restaurant_app/data/model/restaurant_list_respone.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const _baseUrl = "https://restaurant-api.dicoding.dev";
+
+  Future<RestaurantListRespone> getRestaurantList() async {
+    final response = await http.get(Uri.parse("$_baseUrl/list"));
+
+    if (response.statusCode == 200) {
+      return RestaurantListRespone.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load Restaurant List.');
+    }
+  }
+
+  Future<RestaurantDetailRespone> getRestaurantDetail(String id) async {
+    final response = await http.get(Uri.parse("$_baseUrl/detail/$id"));
+
+    if (response.statusCode == 200) {
+      return RestaurantDetailRespone.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load Restaurant Detail.');
+    }
+  }
+}
